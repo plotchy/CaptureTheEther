9562d9c2cc21d54bc45bc8a598b6e933ada7c576
plotchy <plotchy@tutanota.com>
2022-01-31 16:29:14 -0500

    Solved Choose a Nickname

---
+++ scripts/challenges/ChooseNickname/choosenickname.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.insert(1, "/home/plotchy/code/ctf/capturetheether/scripts")
+from helpful_scripts import get_account, padHexTo32Bytes, padHexTo32Bytes
+from brownie import network, config, Contract
+import time
+from web3 import Web3, HTTPProvider
+import json
+from dotenv import dotenv_values
+
+PROJECT_SCRIPTS_PATH = dotenv_values(".env")["CTF_PROJECT_PATH"] + "/scripts/"
+PROJECT_BUILD_PATH = dotenv_values(".env")["CTF_PROJECT_PATH"] + "/build/"
+
+def main():
+    with open(PROJECT_BUILD_PATH + "contracts/CaptureTheEther.json") as f:
+        info_json = json.load(f)
+    CTEabi = info_json["abi"]
+
+    with open(PROJECT_BUILD_PATH + "contracts/NicknameChallenge.json") as f:
+        info_json = json.load(f)
+    checkerabi = info_json["abi"]
+
+
+    CTE_Contract_Address = "0x71c46Ed333C35e4E6c62D32dc7C8F00D125b4fee"
+    nicknameContract = Contract.from_abi(name="CaptureTheEther", address=CTE_Contract_Address,abi=CTEabi)
+    checker_Contract_Address = "0x57AF10539058C68d77E11B542652C8c4a4163896"
+    checkerContract = Contract.from_abi(name="NicknameChallenge", address=checker_Contract_Address,abi=checkerabi)
+
+    account = get_account()
+    myHexName = Web3.toHex(text='plotchy')[2:]
+    myHexName = padHexTo32Bytes(myHexName, "lower")
+    print(myHexName)
+    # name_tx = nicknameContract.setNickname(myHexName, {"from": account})
+    # name_tx.wait(1)
+    isComplete = checkerContract.isComplete()
+    print(isComplete)
+    myName = nicknameContract.nicknameOf("0x2A1e0000010D393CF93F89000000617A00d50000")
+    print(myName)
+
+
+    print('Done')
+
+
+if __name__ == "__main__":
+    main()
