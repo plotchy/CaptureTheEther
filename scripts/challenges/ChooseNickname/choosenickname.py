e1b1074564888f2ebdc737a75d148db9216bedd9
plotchy <plotchy@tutanota.com>
2022-01-31 16:37:18 -0500

    Solved Choose a Nickname

--- scripts/challenges/ChooseNickname/choosenickname.py
+++ scripts/challenges/ChooseNickname/choosenickname.py
@@ -1,14 +1,14 @@
 import sys
-sys.path.insert(1, "/home/plotchy/code/ctf/capturetheether/scripts")
-from helpful_scripts import get_account, padHexTo32Bytes, padHexTo32Bytes
+from dotenv import dotenv_values
+PROJECT_SCRIPTS_PATH = dotenv_values(".env")["CTF_PROJECT_PATH"] + "/scripts/"
+PROJECT_BUILD_PATH = dotenv_values(".env")["CTF_PROJECT_PATH"] + "/build/"
+sys.path.insert(1, PROJECT_SCRIPTS_PATH)
+from helpful_scripts import get_account, padHexTo32Bytes
 from brownie import network, config, Contract
 import time
 from web3 import Web3, HTTPProvider
 import json
-from dotenv import dotenv_values
 
-PROJECT_SCRIPTS_PATH = dotenv_values(".env")["CTF_PROJECT_PATH"] + "/scripts/"
-PROJECT_BUILD_PATH = dotenv_values(".env")["CTF_PROJECT_PATH"] + "/build/"
 
 def main():
     with open(PROJECT_BUILD_PATH + "contracts/CaptureTheEther.json") as f:
